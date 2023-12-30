b47972a203e4ae5e86e81db5ddd20cafdc8b0af6
Umoritelno <125469661+Umoritelno@users.noreply.github.com>
2023-12-30 11:08:12 +0300

    Update sh_achiv_table.lua

--- gamemodes/groundkontrol/gamemode/sh_achiv_table.lua
+++ gamemodes/groundkontrol/gamemode/sh_achiv_table.lua
@@ -8,11 +8,11 @@ GM.Achievements["headshot"] = {
     Reward = {cash = 1000,exp = 50}
 }
 
-GM.Achievements["dlan"] = {
+--[[GM.Achievements["dlan"] = {
     Name = "Неотвратимость карающей длани",
     Desc = "Заставьте врага познать неотвратимость карающей длани с помощью РПГ-7",
     Reward = {cash = 2500,exp = 150}
-}
+}--]]
 
 GM.Achievements["spec"] = {
     Name = "Специалист",
